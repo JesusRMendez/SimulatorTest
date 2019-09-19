6ad515dae236450cfc7e1fffaa5d0f0384adaa95
Jesus R Mendez <Jesusrmendezp@gmail.com>
2019-09-19 10:00:08 -0400

    Se muestra el simulador como corresponde

--- src/App.js
+++ src/App.js
@@ -6,13 +6,10 @@ import BigTeam from './Components/BigTeam/BigTeam';
 import ConfirmationSelected from './Components/ConfirmationSelected/ConfirmationSelected';
 import Thumbail from './Components/Thumbail/Thumbail';
 
-const presidentPhotoIndex=2;
-const regidorPhotoIndex = 18;
-
 class App extends React.Component{
   state = {
     imagesTeam: ["images/abanico.png", "images/prm.png", "./images/ndv.png"],
-    photos: ["images/abanico.png"],
+    photos: ["images/2.jpeg", "images/18.jpg", "luis.jpg"],
     TeamName: ["Partido Abanico", "PRM", "No Deseo Votar"],
     teamSelected: 0,
     viewOrder: ["teamSelected", "teamConfirmation", "presidentSelected"],
@@ -22,16 +19,41 @@ class App extends React.Component{
     currentName: "",
     currentJobTitle: "",
     nextViewName: "",
+    beforeViewName: "",
+    selecteds: []
   }
   nextView(view){
-    this.setState({currentOrder: view});
+    var jobTitle = "";
+    switch(view){
+      case "senador":
+          jobTitle = "SENADOR(A)";
+          break;
+        case "diputado":
+            jobTitle = "DIPUTADO(A)";
+            break;
+        case "regidor":
+            jobTitle = "REGIDOR(A)";
+            break;
+        case "alcalde":
+            jobTitle = "ALCALDE(SA)";
+            break;
+      default:
+          jobTitle = this.state.currentJobTitle;
+        break;
+    }
+    this.setState({currentOrder: view, currentJobTitle: jobTitle});
   }
   setCurrentConfirmation(currentImage, i, nextViewName, name, jobTitle){
+    var selectedConfirmations = this.state.selecteds;
+    selectedConfirmations.push(i);
     this.setState({currentConfirmationImage: currentImage, 
                   currentName: name,
                   currentJobTitle:jobTitle,
                     currentNumber: i, 
-                    nextViewName: nextViewName});
+                    nextViewName: nextViewName, 
+                    beforeViewName: this.state.currentOrder,
+                    selecteds: selectedConfirmations});
+                  
     this.nextView("confirmation");
   }
   generateThumbs(count, image, indexImage, candidateName, positionName, nextViewName){
@@ -41,9 +63,9 @@ class App extends React.Component{
       let currentImage = "";
       let currentCandidateName = "";
       if (i === count){
-        currentImage ="images/none.PNG";
+        currentImage ="images/none.png";
       }else if (i !== indexImage){
-        currentImage ="images/candidato.PNG";
+        currentImage ="images/candidato.png";
       }else{
         currentImage = image;
         currentCandidateName = candidateName;
@@ -56,7 +78,10 @@ class App extends React.Component{
     return thumbails;
   }
   selectTeam(index){
-    this.setState({teamSelected: index, currentOrder: "teamConfirmation"});
+    this.setState({teamSelected: index, currentOrder: "teamConfirmation",  
+                    nextViewName: "presidentSelected", 
+                    beforeViewName: "teamSelected",
+                    currentJobTitle: "PRESIDENTE(A)"});
   }
 
   //Views
@@ -82,17 +107,73 @@ class App extends React.Component{
         number="" name="" jobTitle="" />
   }
   showPresident(){
+
     return (
       <div className="row">
         <div className="col-md-12">
           <div className="row">
-              {this.generateThumbs(16, this.state.photos[0], 2, "Luis Abinader", "PRESIDENTE(A)", "diputadoView")}
+              {this.generateThumbs(16, this.state.photos[2], 1, "Luis Abinader", "PRESIDENTE(A)", "senador")}
           </div>
           </div>
         </div>
     );
   }
 
+  showSenators(){
+    
+    return (
+      <div className="row">
+        <div className="col-md-12">
+          <div className="row">
+              {this.generateThumbs(16, this.state.photos[0], 0, "", "SENADOR(A)", "diputado")}
+          </div>
+          </div>
+        </div>
+    );
+  }
+
+  showDiputados(){
+    return (
+      <div className="row">
+        <div className="col-md-12">
+          <div className="row">
+              {this.generateThumbs(65, this.state.photos[0], 0, "", "DIPUTADO(A)", "alcalde")}
+          </div>
+          </div>
+        </div>
+    );
+  }
+  showAlcades(){
+    return (
+      <div className="row">
+        <div className="col-md-12">
+          <div className="row">
+              {this.generateThumbs(16, this.state.photos[0], 2, "Manuel Jimenez", "ALCALDE(SA)", "regidor")}
+          </div>
+          </div>
+        </div>
+    );
+  }
+  showRegidores(){
+    return (
+      <div className="row">
+        <div className="col-md-12">
+          <div className="row">
+              {this.generateThumbs(65, this.state.photos[1], 18, "Jesús Méndez", "REGIDOR(A)", "end")}
+          </div>
+          </div>
+        </div>
+    );
+  }
+  back(){
+    var selecteds = this.state.selecteds;
+    selecteds.pop();
+    this.setState( {currentOrder: this.state.beforeViewName, selecteds: selecteds});
+    
+  }
+  next(){
+    this.setState( {currentOrder: this.state.nextViewName});
+  }
   viewScreen(){
     console.log(this.state.currentOrder);
     switch(this.state.currentOrder){
@@ -100,6 +181,7 @@ class App extends React.Component{
         return this.showTeam();
       case "teamConfirmation":
         return this.showTeamConfirmation("presidentSelected");
+      
         case "presidentSelected":
             return this.showPresident();
         case "confirmation":
@@ -107,15 +189,28 @@ class App extends React.Component{
                                this.state.currentNumber, this.state.nextViewName, 
                                this.state.currentName,
                                this.state.currentJobTitle);
-          
+        case "senador":
+          return this.showSenators();
+        case "diputado":
+          return this.showDiputados();
+        case "regidor":
+          return this.showRegidores();
+        case "alcalde":
+          return this.showAlcades();
       default:
         return "";
     }
   }
   render(){
+    //Get all candidate codes
+    fetch("https://github.com/JesusRMendez/PRMImges/blob/master/candidate.json").then(function(value){
+      debugger;
+    });
     return (
       <div> 
-        <TitleTeam mode={this.state.currentOrder}/>
+        <TitleTeam mode={this.state.currentOrder} back={this.back.bind(this)} next={this.next.bind(this)}
+                JobTitle={this.state.currentJobTitle} team={this.state.imagesTeam[this.state.teamSelected]}
+                confirmation={this.state.beforeViewName} />
         <br/>
           <div class="col-md-12">
            {this.viewScreen()}
--- src/Components/Headers/HeaderTitle.js
+++ src/Components/Headers/HeaderTitle.js
@@ -1,23 +1,73 @@
 import React from 'react';
 import './HeaderTitle.css';
 
-function HeaderTitle() {
+function HeaderTitle(props) {
+    function showConfirmation(props){
+        if (props.mode === "confirmation" || props.mode === "teamConfirmation")
+        return (
+                <div class="row bg-Index" style={{margin: "0px"}}>
+                    <a className="col-md-3 btn btn-danger" href="#back" onClick={props.back} style={{borderRadius:"0px"}}>
+                        <h3 class="BotonesFont negrita ">RETORNAR</h3></a>
+                        <div className="col-md-6 divPadre">
+                            <div className="divHijo">
+                                <h5 className="negrita margin CentralTexto">TOQUE NUEVAMENTE EL RECUADRO PARA CONFIRMAR</h5>
+                            </div>
+                        </div>
+                        <a className="col-md-3 btn btn-success colorblack" 
+                        href="#next" style={{borderRadius:"0px"}}  onClick={props.next}>
+                            <h3 id="blink" className="luz">CONTINUAR</h3>
+                        </a>
+                </div>
+
+        );
+    }
+
+    function showCabeceraCentradas(props){
+        if (props.mode ==="teamSelected" || props.mode === "teamConfirmation"){
+            return (
+                <div className="row">
+                    <div className="col-md-12 centrado">
+                        <h6 className="hTituloPrincipales negrita height"></h6>
+                        <h2 className="hTituloPrincipales negrita">PARTIDO POLITICO</h2>
+                        <h6 className="parpadea text recuadro">Toque el Recuadro de su Preferencia</h6>
+                    </div>  
+                </div>
+                
+            );
+        }else{
+            return (
+                <div class="row">
+                    <div style={{justifyContent: "flex-start", flexDirection: "row", padding: "0px", margin: "0px",width: "10%"}}>
+                            <img src={props.team} className="img img-thumbnail margin-left" alt="Lights"
+                                    id="imgPartido" style={{width: "100%", height: "100%"}}/>
+                    </div>
+                    <div class="centrado CabezeraCentrada" style={{width: "80%"}}>
+                        <h6 id="Nivel" class="hTituloNivel">Nivel: Nacional</h6>
+                        <h2 class="parpadea text heid">{props.JobTitle}</h2>
+                        <h6 class="parpadea text" style={{fontSize: "12px", margin: "0%"}}>TOQUE EL RECUADRO DE SU PREFERENCIA</h6>
+                    </div>
+                    <div class="derecha" 
+                    style={{padding: "0px", margin: "0px", width: "10%", justifyContent: "flex-end", flexDirection: "row"}}>
+                        <img src={"images/"+(props.mode === "confirmation" ? props.confirmation : props.mode)+".png"}  alt="cargo" style={{width: "100%", height: "100%"}}
+                        className="img img-thumbnail margin-right" id="imgCargo"/>
+                    </div>
+                </div>
+
+                );
+        }
+       
+    }
     return (
         <div>
             <div className="col-md-12">    
                 <div className="row  bg-Index">
                     <div className="col-md-12">
-                        <div className="row">
-                            
-                            <div className="col-md-12 centrado">
-                                <h6 className="hTituloPrincipales negrita height"></h6>
-                                <h2 className="hTituloPrincipales negrita">PARTIDO POLITICO</h2>
-                                <h6 className="parpadea text recuadro">Toque el Recuadro de su Preferencia</h6>
-                            </div>
-                            
-                        </div>
+                    {showCabeceraCentradas(props)}
+                       
                     </div>
                 </div>
+                <div className="clearfix"></div>
+                {showConfirmation(props)}
             </div>
             <br/>
 
